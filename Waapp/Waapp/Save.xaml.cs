74b022133d5df184b1fe904f56f7b3e5552d21e6
GalsCzE <sevca99@gmail.com>
2017-05-31 12:59:47 +0200

    sddfs

--- Waapp/Waapp/Save.xaml.cs
+++ Waapp/Waapp/Save.xaml.cs
@@ -20,6 +20,8 @@ namespace Waapp
             InitializeComponent();
             we = App.Database.GetItemsAsync().Result;
             data.ItemsSource = we;
+
+
         }
 
         private void data_ItemSelected(object sender, SelectedItemChangedEventArgs e)
